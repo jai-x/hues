653e1add6492454e19f09c26dba7319a16f33658
Jai Sharma <jai@jai.moe>
2021-07-17 02:51:49 +0100

    Add `ToString` method to `Image`

--- hues.Game/Image.cs
+++ hues.Game/Image.cs
@@ -1,13 +1,19 @@
+using System;
+
 namespace hues.Game
 {
     public struct Image
     {
         public string Name { get; init; }
         public string Source { get; init; }
-
         public string TexturePath { get; init; }
-
         public Respack Respack { get; init; }
+
+        public override string ToString() =>
+            $"<{nameof(Image)}> " +
+            $"Name: {Name}, " +
+            $"Source: {Source}, " +
+            $"TexturePath: {TexturePath}";
 
         public Image(string path, string name)
         {
