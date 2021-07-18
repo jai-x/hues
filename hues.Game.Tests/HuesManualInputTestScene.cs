991b55b9d4a85763f989bda679449627d51bf1c5
Jai Sharma <jai@jai.moe>
2021-07-18 20:27:10 +0100

    Make base TestScene classes abstract to exclude them from test browser

--- hues.Game.Tests/HuesManualInputTestScene.cs
+++ hues.Game.Tests/HuesManualInputTestScene.cs
@@ -2,7 +2,7 @@ using osu.Framework.Testing;
 
 namespace hues.Game.Tests
 {
-    public class HuesManualInputTestScene : ManualInputManagerTestScene
+    public abstract class HuesManualInputTestScene : ManualInputManagerTestScene
     {
         protected override ITestSceneTestRunner CreateRunner() => new HuesTestSceneTestRunner();
     }
--- hues.Game.Tests/HuesTestScene.cs
+++ hues.Game.Tests/HuesTestScene.cs
@@ -2,7 +2,7 @@ using osu.Framework.Testing;
 
 namespace hues.Game.Tests
 {
-    public class HuesTestScene : TestScene
+    public abstract class HuesTestScene : TestScene
     {
         protected override ITestSceneTestRunner CreateRunner() => new HuesTestSceneTestRunner();
     }
