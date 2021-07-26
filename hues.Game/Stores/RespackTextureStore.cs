1c34c26fb34c39dc981c1128dfffe74160bab104
Jai Sharma <jai@jai.moe>
2021-07-27 00:51:52 +0100

    Check filter mode enums for RespackTextureStore

--- hues.Game/Stores/RespackTextureStore.cs
+++ hues.Game/Stores/RespackTextureStore.cs
@@ -1,12 +1,18 @@
 using osu.Framework.Graphics.Textures;
 using osu.Framework.IO.Stores;
+using osuTK.Graphics.ES30;
 
 namespace hues.Game.Stores
 {
     public class RespackTextureStore : LargeTextureStore
     {
+        // From osuTK/Graphics/ES10/Enum.cs
+        // All.Nearest
+        // All.Linear (default)
+        private const All filterMode = All.Linear;
+
         public RespackTextureStore(IResourceStore<TextureUpload> store)
-            : base(store)
+            : base(store, filterMode)
         { }
     }
 }
