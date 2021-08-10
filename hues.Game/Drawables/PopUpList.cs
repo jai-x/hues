4f70df4ded216e47be98c954c3f7664258ae12fa
Jai Sharma <jai@jai.moe>
2021-08-10 19:01:39 +0100

    Improve visuals of PopUpList

--- hues.Game/Drawables/PopUpList.cs
+++ hues.Game/Drawables/PopUpList.cs
@@ -4,28 +4,21 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Input.Events;
 using osuTK;
 
 namespace hues.Game.Drawables
 {
     public class PopUpList : VisibilityContainer
     {
-        protected override void PopIn()
-        {
-            this.FadeIn(200);
-        }
-
-        protected override void PopOut()
-        {
-            this.FadeOut(200);
-        }
-
+        protected override void PopIn() => this.FadeIn(200);
+        protected override void PopOut() => this.FadeOut(200);
 
         public PopUpList()
         {
             Masking = true;
             BorderColour = Colour4.Black;
-            BorderThickness = 1;
+            BorderThickness = 3;
         }
 
         private FillFlowContainer flow;
@@ -47,7 +40,6 @@ namespace hues.Game.Drawables
                     ScrollbarOverlapsContent = false,
                     Child = flow = new FillFlowContainer
                     {
-                        Spacing = new Vector2(0, 2),
                         Direction = FillDirection.Vertical,
                         RelativeSizeAxes = Axes.X,
                         AutoSizeAxes = Axes.Y,
@@ -56,28 +48,84 @@ namespace hues.Game.Drawables
             };
         }
 
-        protected void ClearFlow()
+        protected void ClearFlow() => flow.Clear();
+
+        private class Spacer : Box
         {
-            flow.Clear();
+            public Spacer()
+            {
+                RelativeSizeAxes = Axes.X;
+                Height = 2;
+                Colour = Colour4.Black;
+            }
+        }
+
+        private class ListLine : CompositeDrawable
+        {
+            private string text;
+            private Action action;
+            private Box background;
+
+            private readonly Colour4 noHoverColour = Colour4.Transparent;
+            private readonly Colour4 hoverColour = Colour4.LightGray.Lighten(5);
+
+            public ListLine(string text, Action action)
+            {
+                this.text = text;
+                this.action = action;
+                RelativeSizeAxes = Axes.X;
+                Height = 12;
+                Padding = new MarginPadding { Horizontal = 3 };
+            }
+
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                InternalChildren = new Drawable[]
+                {
+                    background = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = noHoverColour,
+                    },
+                    new SpriteText
+                    {
+                        Anchor = Anchor.CentreLeft,
+                        Origin = Anchor.CentreLeft,
+                        Text = text,
+                        Colour = Colour4.Black,
+                        Font = FontUsage.Default.With(size: 9),
+                        X = 3,
+                    },
+                };
+            }
+
+            protected override bool OnClick(ClickEvent e)
+            {
+                if (action == null)
+                    return false;
+
+                action.Invoke();
+                return true;
+            }
+
+            protected override void OnHoverLost(HoverLostEvent e) => background.Colour = noHoverColour;
+
+            protected override bool OnHover(HoverEvent e)
+            {
+                background.Colour = hoverColour;
+                return true;
+            }
         }
 
         protected void AddToFlow(string text, Action action)
         {
-            flow.Add(new ClickableContainer
-            {
-                RelativeSizeAxes = Axes.X,
-                AutoSizeAxes = Axes.Y,
-                Action = action,
-                Child = new SpriteText
-                {
-                    Anchor = Anchor.CentreLeft,
-                    Origin = Anchor.CentreLeft,
-                    Text = text,
-                    Colour = Colour4.Black,
-                    Font = FontUsage.Default.With(size: 9),
-                    Margin = new MarginPadding { Horizontal = 3, Vertical = 1 },
-                },
-            });
+            if (flow.Count == 0)
+                flow.Add(new Spacer());
+
+            flow.Add(new ListLine(text, action));
+
+            flow.Add(new Spacer());
         }
     }
 }
