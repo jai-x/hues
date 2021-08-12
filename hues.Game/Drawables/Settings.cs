e6c7b61bfc8e11d4bf8a0a205b17243e5fdb1e73
Jai Sharma <jai@jai.moe>
2021-08-12 19:32:20 +0100

    Improve layout of Settings box

--- hues.Game/Drawables/Settings.cs
+++ hues.Game/Drawables/Settings.cs
@@ -1,8 +1,10 @@
+using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osuTK;
 
 namespace hues.Game.Drawables
 {
@@ -23,11 +25,70 @@ namespace hues.Game.Drawables
 
         public Settings()
         {
-            Height = 400;
+            AutoSizeAxes = Axes.Y;
             Width = 800;
             Masking = true;
             BorderColour = Colour4.Black;
-            BorderThickness = 1;
+            BorderThickness = 3;
+        }
+
+        private class InfoListing : Container
+        {
+            public string Title { get; init; }
+            public string[] Contents { get; init; }
+
+            public InfoListing()
+            {
+                Masking = true;
+                CornerRadius = 15;
+                AutoSizeAxes = Axes.Y;
+                RelativeSizeAxes = Axes.X;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Colour4.Gray.Lighten(0.25f),
+                    },
+                    new FillFlowContainer
+                    {
+                        Direction = FillDirection.Vertical,
+
+                        Anchor = Anchor.TopCentre,
+                        Origin = Anchor.TopCentre,
+
+                        RelativeSizeAxes = Axes.X,
+                        AutoSizeAxes = Axes.Y,
+
+                        Spacing = new Vector2(0, 5),
+                        Padding = new MarginPadding(15),
+
+                        Children = Contents.Select(line => new SpriteText
+                        {
+                            Text = line,
+                            Font = FontUsage.Default.With(size: 11),
+                            Colour = Colour4.Black,
+                            Anchor = Anchor.TopLeft,
+                            Origin = Anchor.TopLeft,
+                        })
+                        .Prepend(new SpriteText
+                        {
+                            Text = Title,
+                            Font = FontUsage.Default.With(size: 14),
+                            Colour = Colour4.Black,
+                            Anchor = Anchor.TopLeft,
+                            Origin = Anchor.TopLeft,
+                            Margin = new MarginPadding { Bottom = 10 },
+                        })
+                        .ToArray(),
+                    },
+                };
+            }
         }
 
         [BackgroundDependencyLoader]
@@ -40,166 +101,96 @@ namespace hues.Game.Drawables
                     Colour = Colour4.LightGray,
                     RelativeSizeAxes = Axes.Both,
                 },
-                new SpriteText
+                new FillFlowContainer
                 {
-                    Text = "hues",
-                    Colour = Colour4.Black,
+                    Direction = FillDirection.Vertical,
+
                     Anchor = Anchor.TopCentre,
-                    Origin = Anchor.Centre,
-                    Font = FontUsage.Default.With(size: 20),
-                    Y = 30,
-                },
-                new SpriteText
-                {
-                    Text = (game.IsReleased ? "v" : "") + game.Version,
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.TopCentre,
-                    Origin = Anchor.Centre,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = 50,
-                },
-                new SpriteText
-                {
-                    Text = $"running on osu-framework v{game.FrameworkVersion}",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.TopCentre,
-                    Origin = Anchor.Centre,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = 65,
-                },
-                new SpriteText
-                {
-                    Text = "drag a respack into the window to load it",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Font = FontUsage.Default.With(size: 14),
-                },
-                new SpriteText
-                {
-                    Text = "F11:",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomRight,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -116,
-                },
-                new SpriteText
-                {
-                    Text = "window mode",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomLeft,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -116,
-                },
-                new SpriteText
-                {
-                    Text = "Ctrl + F1:",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomRight,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -100,
-                },
-                new SpriteText
-                {
-                    Text = "draw visualiser",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomLeft,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -100,
-                },
-                new SpriteText
-                {
-                    Text = "Ctrl + F2:",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomRight,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -84,
-                },
-                new SpriteText
-                {
-                    Text = "global statistics",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomLeft,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -84,
-                },
-                new SpriteText
-                {
-                    Text = "Ctrl + F3:",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomRight,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -68,
-                },
-                new SpriteText
-                {
-                    Text = "texture visualiser",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomLeft,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -68,
-                },
-                new SpriteText
-                {
-                    Text = "Ctrl + F9:",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomRight,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -52,
-                },
-                new SpriteText
-                {
-                    Text = "audio mixer",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomLeft,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -52,
-                },
-                new SpriteText
-                {
-                    Text = "Ctrl + F10:",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomRight,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -36,
-                },
-                new SpriteText
-                {
-                    Text = "debug log",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomLeft,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -36,
-                },
-                new SpriteText
-                {
-                    Text = "Ctrl + F11:",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomRight,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -20,
-                },
-                new SpriteText
-                {
-                    Text = "performance/FPS display",
-                    Colour = Colour4.Black,
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomLeft,
-                    Font = FontUsage.Default.With(size: 12),
-                    Y = -20,
+                    Origin = Anchor.TopCentre,
+
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+
+                    Padding = new MarginPadding { Vertical = 10, Horizontal = 150 },
+
+                    Spacing = new Vector2(0, 10),
+
+                    Children = new Drawable[]
+                    {
+                        new SpriteText
+                        {
+                            Text = "hues",
+                            Colour = Colour4.Black,
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            Font = FontUsage.Default.With(size: 30),
+                        },
+                        new SpriteText
+                        {
+                            Text = (game.IsReleased ? "v" : "") + game.Version,
+                            Colour = Colour4.Black,
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.Centre,
+                            Font = FontUsage.Default.With(size: 12),
+                        },
+                        new SpriteText
+                        {
+                            Text = $"running on osu-framework v{game.FrameworkVersion}",
+                            Colour = Colour4.Black,
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.Centre,
+                            Font = FontUsage.Default.With(size: 12),
+                            Y = 65,
+                        },
+                        new SpriteText
+                        {
+                            Text = "Drag a respack into the window to load it!",
+                            Colour = Colour4.Black,
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.Centre,
+                            Font = FontUsage.Default.With(size: 14),
+                            Y = 65,
+                        },
+                        new Box
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            Height = 3,
+                            Colour = Colour4.Black,
+                        },
+                        new InfoListing
+                        {
+                            Title = "Beat glossary",
+                            Contents = new string[]
+                            {
+                                "x Vertical blur (snare)",
+                                "o Horizontal blur (bass)",
+                                "- No blur",
+                                "+ Blackout",
+                                "¤ Whiteout",
+                                "| Short blackout",
+                                ": Color only",
+                                "* Image only",
+                                "X Vertical blur only",
+                                "O Horizontal blur only",
+                                "i Invert all colours",
+                                "I Invert & change image",
+                            },
+                        },
+                        new InfoListing
+                        {
+                            Title = "Keybindings",
+                            Contents = new string[]
+                            {
+                                "       F11: Window Mode",
+                                " Ctrl + F1: Draw Visualiser",
+                                " Ctrl + F2: Global Statistics",
+                                " Ctrl + F3: Texture Visualiser",
+                                " Ctrl + F9: Audio Mixer",
+                                "Ctrl + F10: Debug Log Overlay",
+                                "Ctrl + F11: FPS/Performance Overlay",
+                            },
+                        },
+                    },
                 },
             };
         }
