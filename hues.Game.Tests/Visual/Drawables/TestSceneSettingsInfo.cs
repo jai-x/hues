7dd7ce57c224586c111cdd0d784bf7545eb38743
Jai Sharma <jai@jai.moe>
2021-08-16 18:03:31 +0100

    Add a test scene for SettingsInfo

---
+++ hues.Game.Tests/Visual/Drawables/TestSceneSettingsInfo.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using hues.Game.Drawables;
+
+namespace hues.Game.Tests.Visual.Drawables
+{
+    [TestFixture]
+    public class TestSceneSettingsInfo : HuesTestScene
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                },
+                new SettingsInfo
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                },
+            };
+        }
+
+        [Test]
+        public void TestVisual()
+        { }
+    }
+}
